1e25aa36707e7e1a7ec6de06a9209d1c2dfed5e3
UtsavJ23 <utsav23jha@gmail.com>
2025-05-15 06:46:23 +0530

    upda

--- backend/config/url.js
+++ backend/config/url.js
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production'
-const url = dev ? process.env.CLIENT_URL : ''
+const url = dev ? process.env.CLIENT_URL : 'https://scaleupaxis.live'
 
 module.exports = url
